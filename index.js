b6ea528ee7f23e1a83665e6d09bf9b78be0d84af
David Leuliette <dleuliette@gmail.com>
2018-10-27 17:30:44 +0100

    Add alfy to fetch datas

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const alfy = require('alfy')
+
+alfy.fetch('jsonplaceholder.typicode.com/posts').then(data => {
+  const items = alfy.inputMatches(data, 'title').map(x => ({
+    title: x.title,
+    subtitle: x.body,
+    arg: x.id,
+  }))
+
+  alfy.output(items)
+})
